74c56d997c311930556bec1b7d3861b986b4b55c
Andrew Carr <acarr@healthwise.org>
2019-11-27 09:54:22 -0700

    Attempting to add in a standalone react app

---
+++ ToDoFunctions/www/js/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div class="jumbotron" style="background-color: white">
+                <h2>To Do List Using Azure Functions</h2>
+            </div>
+        );
+    }
+}
