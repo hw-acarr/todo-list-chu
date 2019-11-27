bb60d9d2599cb145f1057a5c87ded1132e561862
Andrew Carr <acarr@healthwise.org>
2019-11-27 11:00:51 -0700

    Attempting to offload the jsx into another file

---
+++ ToDoFunctions/www/js/Titlebar.jsx
@@ -0,0 +1,9 @@
+class TitleBar extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h2>To Do List Using Azure Functions</h2>
+            </div>
+        );
+    }
+}
