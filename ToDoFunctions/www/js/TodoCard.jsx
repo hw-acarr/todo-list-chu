b8e2380680610f70a1c55d1f4d42c7be6fa6962b
Andrew Carr <acarr@healthwise.org>
2019-12-10 13:43:24 -0700

    Once more, with feeling

--- ToDoFunctions/www/js/TodoCard.jsx
+++ ToDoFunctions/www/js/TodoCard.jsx
@@ -66,7 +66,7 @@ class BasicTaskActions extends React.Component {
     render() {
         return (
             <div className="task-actions">
-                <button name="modify" type="button" onClick={Edit(this.props.key)}  >Modify</button>
+                <button name="modify" type="button" onClick={() => Edit(this.props.key)}  >Modify</button>
                 <button name="complete" type="button">Complete</button>
             </div>
         );
