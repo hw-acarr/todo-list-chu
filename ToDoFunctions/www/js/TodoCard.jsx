9c4f63d37ced6f3da8760a522b17650db5acdfba
Andrew Carr <acarr@healthwise.org>
2019-12-10 09:09:48 -0700

    Added basic card

--- ToDoFunctions/www/js/TodoCard.jsx
+++ ToDoFunctions/www/js/TodoCard.jsx
@@ -18,6 +18,49 @@ class TodoCard extends React.Component {
     };
 }
 
+class BasicCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+
+    render() {
+        return (
+            <div className="taskcard">
+                <BasicTaskDescription />
+                <BasicTaskActions />
+            </div>
+        );
+    }
+}
+
+class BasicTaskDescription extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+    render() {
+        return (
+            <div className="taskdescription">
+                <div>Title: {this.props.item.title}</div>
+                <div>Description: {this.props.item.description}</div>
+                <div>Due Date: {this.props.item.duedate}</div>
+                <div>Priority: {this.props.item.priority}</div>
+            </div>
+        );
+    };
+}
+
+function BasicTaskActions() {
+    return (
+        <div className="taskactions">
+            <button name="Modify" />
+            <button name="Complete" />
+        </div>
+    );
+}
+
+
 function TodoCardList() {
     const [items, setItems] = React.useState([]);
     const [hasLoaded, setLoadedStatus] = React.useState(false);
@@ -39,6 +82,9 @@ function TodoCardList() {
         <div>
             {hasLoaded && items.map((item, index) => (
                 <TodoCard key={item.id} title={item.title} description={item.description} />
+                <hr></hr>
+                <BasicCard key={item.id} item={item} />
+                <hr></hr>
             ))}
         </div>
     );
